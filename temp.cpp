f4101d8ab6ffad9044f54dd29893b9dcae87f55e
CanyonSkull-OS <omershahid78@gmail.com>
2025-04-01 20:42:13 +0500

    Oonga boonga project risk

---
+++ temp.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML 3.0.0 Test");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(350.f, 250.f);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
